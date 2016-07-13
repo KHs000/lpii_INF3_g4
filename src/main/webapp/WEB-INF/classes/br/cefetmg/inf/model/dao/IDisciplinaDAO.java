64c69d24fd6bcc778984f3f480499a20eb61a92b
Felipe Rabelo <carbonemasseo@hotmail.com>
2016-07-12 23:23:20 -0300

    CDD IDAO
    Correção de commit demente parte 2

---
+++ src/main/webapp/WEB-INF/classes/br/cefetmg/inf/model/dao/IDisciplinaDAO.java
@@ -0,0 +1,14 @@
+
+package WEB-INF.classes.br.cefetmg.inf.model.dao;
+
+import br.cefetmg.inf.model.domain.Disciplina;
+import br.cefetmg.inf.util.db.persistence.GenericNamedDAO;
+
+
+/**
+ *
+ * @author Felipe Rabelo
+ */
+public interface IDisciplinaDAO extends GenericNamedDAO<Disciplina>{
+    
+}
