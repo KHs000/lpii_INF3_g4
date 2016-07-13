0e64b02289f747491e2d1c44c9ed2fa6de3e321f
Felipe Rabelo <carbonemasseo@hotmail.com>
2016-07-12 23:24:57 -0300

    CDD Service
    Correção de commit demente parte 4

---
+++ src/main/webapp/WEB-INF/classes/br/cefetmg/inf/model/service/impl/ManterDisciplina.java
@@ -0,0 +1,28 @@
+package WEB-INF.classes.br.cefetmg.inf.model.service.impl;
+
+import br.cefetmg.inf.model.domain.Disciplina;
+import br.cefetmg.inf.util.db.exception.NegocioException;
+import br.cefetmg.inf.util.db.exception.PersistenciaException;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author Felipe Rabelo
+ */
+public class ManterDisciplina implements IManterDisciplina {
+
+    public void cadastrar(Disciplina disciplina) throws PersistenciaException, NegocioException {
+        IDisciplinaDAO disciplinaDAO = new DisciplinaDAO();
+        Long disciplinaId = disciplinaDAO.inserir(disciplina);
+        disciplina.setId(disciplinaId);
+    }
+
+    public List<Disciplina> listarTodos() throws PersistenciaException, NegocioException {
+        IDisciplinaDAO disciplinaDAO = new DisciplinaDAO();
+        List<Disciplina> listDisciplina = disciplinaDAO.listarTodos();
+        return listDisciplina;
+    }
+    
+}
