8f17b7c544eb60f88d8f621b394db69f0c3e3452
98732 <rfr0067@hotmail.com>
2016-07-13 14:22:17 -0300

    Novo BD e exceções
    -Servidor de BD alterado para o Heroku
    -Exceções de persistência e de Regra de Negócio adicionadas

--- ScMcOrE/src/db/PostgresqlConnection.java
+++ ScMcOrE/src/db/PostgresqlConnection.java
@@ -7,27 +7,28 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PostgresqlConnection implements ConnectionFactory {
-
+    
+    private final static String ServerUrl = "ec2-54-243-203-143.compute-1.amazonaws.com";
     private final static String dbDriver = "org.postgresql.Driver";
-    private final static String dbURL = "jdbc:postgresql://localhost:5433/sislocdb";
-    private final static String user = "postgres";
-    private final static String pass = "postgres";
-
+    private final static String DbName = "d2bq4n4gih8m47";
+    private final static String user = "mlbewuwxipqdxe";
+    private final static String pass = "AFkHnl5GAInDV18d1xVVEQzfuL";
+    private final static String connectionUrl =
+            "jdbc:postgresql://"+ ServerUrl + ":"  +
+                    "5432" + "/" + DbName +
+                    "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" +  // Forca o uso de SSL
+                    "&user=" + user +
+                    "&password=" + pass ;
+    
     public PostgresqlConnection() {
+    
     }
 
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName(dbDriver);
-        return DriverManager.getConnection(dbURL, user, pass);
+        
+        return DriverManager.getConnection(connectionUrl);
     }
     
-    public static void main(String[] args) {
-        try {
-            ConnectionFactory cf = new PostgresqlConnection();            
-            cf.getConnection();
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(PostgresqlConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 }
